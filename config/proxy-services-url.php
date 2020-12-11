32d7f13ff1417dc79594afe5d1c68d60b68f8eef
Omid Alizadeh <om_alizadeh@yahoo.com>
2020-12-11 16:23:41 +0330

    fix: undefined Server variable

--- config/proxy-services-url.php
+++ config/proxy-services-url.php
@@ -1,15 +1,16 @@
 <?php
 
 return [
-    /**
-     * 
-     * Services List and Their URLs
-     * 
-     **/
+    /*
+    * 
+    * Services List and Their URLs
+    * 
+    */
+    
     // 'USER'      => "user-service/",
     // 'PRODUCT'   => "product-service/",
     // 'TICKET'    => "ticket-service/",
     // 'SMS'       => "sms-service/"
 
-    'GLOBAL_APP_URL' => env('GLOBAL_APP_URL', env('HTTP_HOST'))
+    'GLOBAL_APP_URL' => env('GLOBAL_APP_URL', url(env('HTTP_HOST')))
 ];
