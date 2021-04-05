1551940bcbeb1d1a4fedd4ee0c4004d8b82cd395
Omid Alizadeh <om_alizadeh@yahoo.com>
2021-04-05 20:54:12 +0430

    fix: Proxy phpdoc for v2.0.0

--- src/BSProxy.php
+++ src/BSProxy.php
@@ -7,21 +7,21 @@ use Illuminate\Support\Facades\Facade;
 /**
  * Class BSProxy
  * @package BSProxy
- * @method static BSProxyResponse|string|mixed makeRequest($request, $service, $method = 'get', $path = null, $modelId = null, $data = [], $headers = [])
+ * @method static BSProxyResponse|mixed makeRequest($request, string $service, ?string $method = null, ?string $path = null, ?int $modelId = null, array $data = [], array $headers = [])
+ * @method static BSProxyResponse|mixed request(string $service)
  * @method static setService($service, $app = 'global_app_url')
- * @method static Proxy withoutException()
- * @method static Proxy setData($data)
- * @method static Proxy withProxyResponse()
- * @method static Proxy addHeader(array $header)
- * @method static Proxy setToken($token = null)
- * @method static Proxy setHeaders(array $headers)
+ * @method static Proxy setData(array $data)
+ * @method static Proxy addHeaders(array $headers)
+ * @method static Proxy setToken($token)
  * @method static Proxy setPath(string $path)
  * @method static Proxy setRequest($request)
  * @method static Proxy setMethod(string $method)
- * @method static Proxy setModelId($modelId)
+ * @method static Proxy setModelId(?int $modelId)
  * @method static mixed|BSProxyResponse dispatch($serviceName)
  * @method static Proxy addFile($name, $file) send file with $name in request
  * @method static string getServiceRequestUrl()
+ * @method static Proxy withoutException()
+ * @method static Proxy withProxyResponse()
  */
 class BSProxy extends Facade
 {
