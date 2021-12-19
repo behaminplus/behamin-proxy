a9d898c79ebd9958fb529aaaebb14229569f88d2
Omid <mohamad.talkhaby@gmail.com>
2021-12-19 14:47:17 +0330

    feat: async requests ProxyResponse

--- src/Responses/ProxyResponse.php
+++ src/Responses/ProxyResponse.php
@@ -46,85 +46,19 @@ class ProxyResponse implements Jsonable, Responsable, ArrayAccess, Arrayable
         return $this->json()['data']['count'];
     }
 
-    public function response(): HttpResponse
-    {
-        return $this->response;
-    }
-
-    public function onSuccess(Closure $closure): ProxyResponse
-    {
-        if ($this->response->successful()) {
-            $closure($this);
-        }
-
-        return $this;
-    }
-
-    public function onDataSuccess(Closure $closure): ProxyResponse
-    {
-        if ($this->response->successful()) {
-            $closure($this->data());
-        }
-
-        return $this;
-    }
-
-    public function onCollectionSuccess(Closure $closure): ProxyResponse
-    {
-        if ($this->response->successful()) {
-            $closure($this->items(), $this->count());
-        }
-
-        return $this;
-    }
-
-    public function onError(Closure $closure): ProxyResponse
-    {
-        if ($this->response->failed()) {
-            $closure($this->toException());
-        }
-
-        return $this;
-    }
-
     public function json()
     {
-        return $this->response->json();
+        return $this->response()->json();
     }
 
-    /**
-     * Create an exception if a server or client error occurred.
-     *
-     * @return ProxyException|void
-     */
-    public function toException(): ProxyException
+    public function status(): int
     {
-        if ($this->response->failed()) {
-            return new ProxyException($this);
-        }
-    }
-
-    /**
-     * Throw an exception if a server or client error occurred.
-     *
-     * @return $this
-     */
-    public function throw(?Closure $closure = null): ProxyResponse
-    {
-        if ($this->response->failed()) {
-            throw tap($this->toException(), function ($exception) use ($closure) {
-                if (!is_null($closure)) {
-                    $closure($this, $exception);
-                }
-            });
-        }
-
-        return $this;
+        return $this->response()->status();
     }
 
     public function jsonResponse(): JsonResponse
     {
-        return response()->json($this->response()->json(), $this->response->status());
+        return response()->json($this->response()->json(), $this->status());
     }
 
     public function toJson($options = 0)
@@ -137,37 +71,108 @@ class ProxyResponse implements Jsonable, Responsable, ArrayAccess, Arrayable
         return $this->json();
     }
 
-    public function toResponse($request)
+    public function onSuccess(Closure $closure): ProxyResponse
     {
-        if ($this->response->header('Content-Type') === 'application/json') {
-            return response()->json($this->json(), $this->response->status());
+        if ($this->response()->successful()) {
+            $closure($this);
         }
 
-        return response($this->response()->body(), $this->response->status());
+        return $this;
+    }
+
+    public function onDataSuccess(Closure $closure): ProxyResponse
+    {
+        if ($this->response()->successful()) {
+            $closure($this->data());
+        }
+
+        return $this;
+    }
+
+    public function onCollectionSuccess(Closure $closure): ProxyResponse
+    {
+        if ($this->response()->successful()) {
+            $closure($this->items(), $this->count());
+        }
+
+        return $this;
+    }
+
+    public function onError(Closure $closure): ProxyResponse
+    {
+        if ($this->response()->failed()) {
+            $closure($this->toException());
+        }
+
+        return $this;
+    }
+
+    /**
+     * Create an exception if a server or client error occurred.
+     *
+     * @return ProxyException|void
+     */
+    public function toException(): ProxyException
+    {
+        if ($this->response()->failed()) {
+            return new ProxyException($this);
+        }
+    }
+
+    /**
+     * Throw an exception if a server or client error occurred.
+     *
+     * @return $this
+     */
+    public function throw(?Closure $closure = null): ProxyResponse
+    {
+        if ($this->response()->failed()) {
+            throw tap($this->toException(), function ($exception) use ($closure) {
+                if (!is_null($closure)) {
+                    $closure($this, $exception);
+                }
+            });
+        }
+
+        return $this;
+    }
+
+    public function toResponse($request)
+    {
+        if ($this->response()->header('Content-Type') === 'application/json') {
+            return response()->json($this->json(), $this->response()->status());
+        }
+
+        return response($this->response()->body(), $this->response()->status());
     }
 
     public function offsetExists($offset): bool
     {
-        return $this->response->offsetExists($offset);
+        return $this->response()->offsetExists($offset);
     }
 
     public function offsetGet($offset)
     {
-        return $this->response->offsetGet($offset);
+        return $this->response()->offsetGet($offset);
     }
 
     public function offsetSet($offset, $value): void
     {
-        $this->response->offsetSet($offset, $value);
+        $this->response()->offsetSet($offset, $value);
     }
 
     public function offsetUnset($offset): void
     {
-        $this->response->offsetUnset($offset);
+        $this->response()->offsetUnset($offset);
     }
 
     public function collect(): Collection
     {
-        return $this->response->collect();
+        return $this->response()->collect();
+    }
+
+    public function response(): HttpResponse
+    {
+        return $this->response;
     }
 }
