f3fd7b530a58d4f2019d2fb10776f5e89f0aa333
Omid <mohamad.talkhaby@gmail.com>
2021-12-19 14:53:10 +0330

    refactor: use jsonResponse

--- src/Responses/ProxyResponse.php
+++ src/Responses/ProxyResponse.php
@@ -140,10 +140,10 @@ class ProxyResponse implements Jsonable, Responsable, ArrayAccess, Arrayable
     public function toResponse($request)
     {
         if ($this->response()->header('Content-Type') === 'application/json') {
-            return response()->json($this->json(), $this->response()->status());
+            return $this->jsonResponse();
         }
 
-        return response($this->response()->body(), $this->response()->status());
+        return response($this->response()->body(), $this->status());
     }
 
     public function offsetExists($offset): bool
